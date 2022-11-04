d09f6d9e2ea19537ed07f4ecced050f26398226e
Valter Balegas <balegas@live.com>
2022-11-04 09:45:32 +0000

    [WIP] Web connect/disconnect button for testing

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,6 +6,9 @@ import './style.css'
 import { initElectricSqlJs, ElectrifiedDatabase} from "electric-sql/browser"
 import { ElectricProvider, useElectric, useElectricQuery } from 'electric-sql/react'
 
+type ConnectivityStatus = "connected" | "disconnected"
+
+const dbName = 'example.db'
 const worker = new Worker("./worker.js", { type: "module" });
 
 export const ElectrifiedExample = () => {
@@ -14,7 +17,7 @@ export const ElectrifiedExample = () => {
   useEffect(() => {
     const init = async () => {
       const SQL = await initElectricSqlJs(worker, {locateFile: (file: string) => `/${file}`})
-      const electrified = await SQL.openDatabase('example.db')
+      const electrified = await SQL.openDatabase(dbName)
 
       setDb(electrified)
     }
@@ -30,6 +33,10 @@ export const ElectrifiedExample = () => {
 }
 
 const ExampleComponent = () => {
+  // Note: this presumes network connectivity is available.
+  // In the future we actually check the state of the network.
+  const [ connectivity, setConnectivity ] = useState<ConnectivityStatus>("connected")
+
   const { results, error } = useElectricQuery('SELECT value FROM items', [])
   const db = useElectric() as ElectrifiedDatabase
 
@@ -55,6 +62,12 @@ const ExampleComponent = () => {
 
   const clearItems = () => {
     db.exec('DELETE FROM items where true')
+  } 
+  
+  const toggleConnectivity = () => {
+    const value = connectivity == 'connected' ? 'disconnected' : 'connected' 
+    db.electric.connectivityChange(value, dbName)
+    setConnectivity(value)
   }  
 
   return (
@@ -70,6 +83,9 @@ const ExampleComponent = () => {
       </button>
       <button className='button' onClick={clearItems}>
       <p className='text'>Clear</p>
+      </button>
+      <button className='button' onClick={toggleConnectivity}>
+      <p className='text'>{connectivity}</p>
       </button>
     </div>
   )
